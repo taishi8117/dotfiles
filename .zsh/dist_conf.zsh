d4755c730d75b24e7cb73480b5927088210bf6d2
Taishi Nojima <taishinojima@devvm32872.prn1.facebook.com>
2017-06-23 22:27:32 -0700

    fixed issue

--- .zsh/dist_conf.zsh
+++ .zsh/dist_conf.zsh
@@ -1,28 +1,22 @@
 #!/bin/zsh
 
-if [ "$(uname)" == "Darwin" ]; then
+if [ "$(uname)" = 'Darwin' ]; then
     # Do something under Mac OS X platform        
-    # echo "Using Darwin"
+    echo "Using Darwin"
     alias lst='ls -ltr -G'
     alias l='ls -ltr -G'
     alias ls='ls -G'
     alias la='ls -la -G'
     alias ll='ls -l -G'
     chpwd() { ls -ltr -G } # cdの後にlsを実行
-elif [ "$(expr substr $(uname -s) 1 5)" == "Linux" ]; then
+elif [ "$(expr substr $(uname -s) 1 5)" = 'Linux' ];  then
     # Do something under GNU/Linux platform
+    echo "Using Linux"
     alias lst='ls -ltr --color=auto'
     alias l='ls -ltr --color=auto'
     alias ls='ls --color=auto'
     alias la='ls -la --color=auto'
     alias ll='ls -l --color=auto'
     chpwd() { ls -ltr --color=auto } # cdの後にlsを実行
-    echo "Using Linux"
-elif [ "$(expr substr $(uname -s) 1 10)" == "MINGW32_NT" ]; then
-    # Do something under 32 bits Windows NT platform
-    echo "Using WIN 32 NT"
-elif [ "$(expr substr $(uname -s) 1 10)" == "MINGW64_NT" ]; then
-    # Do something under 64 bits Windows NT platform
-    echo "Using WIN 64 NT"
 fi
 
--- .zshrc
+++ .zshrc
@@ -8,7 +8,6 @@
 
 source ~/.zsh/git.zsh
 source ~/.zsh/ls_color/ls_color.sh
-source ~/.zsh/dist_conf.zsh
 
 # 色を使用
 autoload -Uz colors
@@ -38,7 +37,26 @@ alias -g G='| grep'
 alias -g GI='| grep -ri'
 
 # エイリアス
-# ls related alias are located under .zsh/dist_conf.zsh
+if [ "$(uname)" = 'Darwin' ]; then
+    # Do something under Mac OS X platform        
+    # echo "Using Darwin"
+    alias lst='ls -ltr -G'
+    alias l='ls -ltr -G'
+    alias ls='ls -G'
+    alias la='ls -la -G'
+    alias ll='ls -l -G'
+    chpwd() { ls -ltr -G } # cdの後にlsを実行
+elif [ "$(expr substr $(uname -s) 1 5)" = 'Linux' ];  then
+    # Do something under GNU/Linux platform
+    # echo "Using Linux"
+    alias lst='ls -ltr --color=auto'
+    alias l='ls -ltr --color=auto'
+    alias ls='ls --color=auto'
+    alias la='ls -la --color=auto'
+    alias ll='ls -l --color=auto'
+    chpwd() { ls -ltr --color=auto } # cdの後にlsを実行
+fi
+
 alias so='source'
 alias v='vim'
 alias vi='vim'
