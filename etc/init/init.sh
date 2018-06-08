00e57b1a851734ef1898fb73474c45106696e057
TAISHI <taishi.nojima@yale.edu>
2018-06-08 03:10:11 -0400

    TMux added

--- etc/init/init.sh
+++ etc/init/init.sh
@@ -3,3 +3,9 @@
 echo "[+] Installing vim plugins..."
 vim +NeoBundleInstall +qall
 echo "[+] Vim plugin installed!"
+
+echo "[+] Installing tmux configurations..."
+DOTFILES_ROOT=$(git rev-parse --show-toplevel)
+TMUX_INSTALL="$DOTFILES_ROOT/tmux-config/install.sh"
+$TMUX_INSTALL
+echo "[+] tmux configuration installed!"
