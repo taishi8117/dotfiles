baf67eac94d1d2713cbc3296934ca9b0b8f98aeb
Taishi Nojima <taishi.nojima@aya.yale.edu>
2019-07-16 00:16:42 -0700

    Some improvement

--- zsh/aliases/_common.zsh
+++ zsh/aliases/_common.zsh
@@ -16,20 +16,19 @@ if [ "$(uname)" = 'Darwin' ]; then
     # echo "Using Darwin"
     export CLICOLOR=1
     export LSCOLORS=GxFxCxDxBxegedabagaced
-    alias lst='ls -ltr -G'
-    alias l='ls -ltr -G'
     alias ls='ls -G'
-    alias la='ls -la -G'
-    alias ll='ls -l -G'
+    alias lst='ls -ltr'
+    alias l='ls -ltr'
+    alias la='ls -la'
+    alias ll='ls -l'
     chpwd() { ls -ltr -G } # cdの後にlsを実行
 elif [ "$(expr substr $(uname -s) 1 5)" = 'Linux' ];  then
     # Do something under GNU/Linux platform
     # echo "Using Linux"
-    alias lst='ls -ltr --color=auto'
-    alias l='ls -ltr --color=auto'
     alias ls='ls --color=auto'
-    alias la='ls -la --color=auto'
-    alias ll='ls -l --color=auto'
+    alias l='ls -alhtr'
+    alias la='ls -la'
+    alias ll='ls -l'
     chpwd() { ls -ltr --color=auto } # cdの後にlsを実行
 fi
 
