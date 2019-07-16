a8e964af24d3d199f2a62f6160a75c15814e40f5
Taishi Nojima <taishi.nojima@aya.yale.edu>
2019-07-15 18:32:18 -0700

    wip reorg

--- zsh/theme.zsh
+++ zsh/theme.zsh
@@ -1,3 +1,12 @@
+# Virtual env
+
+function activate { export VIRTUAL_ENV_DISABLE_PROMPT='1' source ./$1/bin/activate }
+function virtualenv_info {
+    [ $VIRTUAL_ENV ] && echo '('`basename $VIRTUAL_ENV`') '
+  }
+
+
+
 # =========================================================================== #
 # pygmalion PROMPT theme
 # =========================================================================== #
