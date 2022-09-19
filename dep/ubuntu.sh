a6e9ae73994f730e8ce58ff19c4cc4599126705f
Taishi Nojima <taishi.nojima@aya.yale.edu>
2022-09-19 15:23:06 +0900

    neovim install

--- dep/ubuntu.sh
+++ dep/ubuntu.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 
 sudo apt-get update && sudo apt-get install software-properties-common -y
-# sudo add-apt-repository ppa:neovim-ppa/stable
 sudo apt-get update
-sudo apt-get install git sudo zsh tmux neovim python3 python3-dev python3-pip wget gawk curl ripgrep fd-find -y
+sudo apt-get install git sudo zsh tmux python3 python3-dev python3-pip wget gawk curl ripgrep fd-find -y
+
+sudo wget https://github.com/neovim/neovim/releases/download/v0.7.2/nvim-linux64.deb -O /tmp/nvim0.7.2.deb
+sudo apt-get install /tmp/nvim0.7.2.deb
