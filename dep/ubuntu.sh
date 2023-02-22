79c86da140e95a799f417085dc54d2b559748eaa
Taishi <taishi.nojima@aya.yale.edu>
2023-02-21 21:58:51 -0800

    feat(dep): upgrade nvim to 8.3

--- dep/ubuntu.sh
+++ dep/ubuntu.sh
@@ -4,5 +4,5 @@ sudo apt-get update && sudo apt-get install software-properties-common -y
 sudo apt-get update
 sudo apt-get install git sudo zsh tmux python3 python3-dev python3-pip wget gawk curl ripgrep fd-find -y
 
-sudo wget https://github.com/neovim/neovim/releases/download/v0.7.2/nvim-linux64.deb -O /tmp/nvim0.7.2.deb
-sudo apt-get install /tmp/nvim0.7.2.deb
+sudo wget https://github.com/neovim/neovim/releases/download/v0.8.3/nvim-linux64.deb -O /tmp/nvim0.8.3.deb
+sudo apt-get install /tmp/nvim0.8.3.deb
