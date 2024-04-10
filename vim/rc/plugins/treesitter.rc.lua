818d87c197d77a5b7d383e320f057c67ca67526c
Taishi <taishi.nojima@aya.yale.edu>
2024-04-10 18:29:02 -0400

    feat(dotfiles): more stuff

--- vim/rc/plugins/treesitter.rc.lua
+++ vim/rc/plugins/treesitter.rc.lua
@@ -1,6 +1,6 @@
 require('nvim-treesitter.configs').setup {
   ensure_installed = {
-      "c", "lua", "vim", "javascript", "typescript",
+      "c", "lua", "vim", "javascript", "typescript", "org",
       "bash", "css", "dockerfile", "go", "html", "jq", "make", "markdown",
       "python", "regex", "rust", "toml", "yaml", "solidity", "sql", "terraform"
   },
