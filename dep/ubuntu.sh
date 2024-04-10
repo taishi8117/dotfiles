d02a4c3443408968ddb29bbd36f88a5b9e41896f
Taishi <taishi.nojima@aya.yale.edu>
2024-04-10 18:30:45 -0400

    feat(dep): upgrade neovim to 0.9.5

--- dep/ubuntu.sh
+++ dep/ubuntu.sh
@@ -6,3 +6,7 @@ sudo apt-get install git sudo zsh tmux python3 python3-dev python3-pip wget gawk
 
 sudo wget https://github.com/neovim/neovim/releases/download/v0.8.3/nvim-linux64.deb -O /tmp/nvim0.8.3.deb
 sudo apt-get install /tmp/nvim0.8.3.deb
+
+sudo wget https://github.com/neovim/neovim/releases/download/v0.9.5/nvim-linux64.tar.gz -O /tmp/nvim0.9.5.tar.gz
+sudo tar -xzvf /tmp/nvim0.9.5.tar.gz -C /tmp
+sudo cp /tmp/nvim-linux64/bin/nvim /usr/local/bin/nvim
